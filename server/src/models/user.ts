19220eb5c26846dd06c0daf2dbdfb313249c0950
MaverickMystic <khanthtoomaung198@gmail.com@example.com>
2026-06-05 20:08:07 +0630

    Initial commit: full client and server project

--- server/src/models/user.ts
+++
@@ -1,8 +0,0 @@
-import mongoose, { Schema } from "mongoose";
-
-const UserSchema=new Schema({
-    username:{type:String,required:true,unique:true},
-    password:{type:String,required:true},
-})
-
-export default mongoose.model('User',UserSchema);
